73f1b14b518232c3e14f2cae6b1359bb0f107037
Freund Lisa <lisa.freund@stud.fh-campuswien.ac.at>
2024-04-29 16:01:23 +0200

    Added classes for Data-Layer

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/MovieEntity.java
@@ -0,0 +1,13 @@
+package at.ac.fhcampuswien.fhmdb.models;
+
+public class MovieEntity {
+    long id;
+    String apiId;
+    String title;
+    String description;
+    String genres;
+    int releaseYear;
+    String imgUrl;
+    int lengthInMinutes;
+    double rating;
+}
